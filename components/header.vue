5aed2d16115aa4429e8e08cb3302e3c09699757f
unknown <kidosan3.14@gmail.com>
2021-06-16 21:05:45 +0900

    add pages and fix header

--- components/header.vue
+++ components/header.vue
@@ -1,5 +1,57 @@
 <template>
   <div class="header1">
-    <h1>header</h1>
+    <h1>
+      <a href="/">YuKiMuMa</a>
+    </h1>
+    <nav class="pc-nav">
+        <ul>
+            <li><a href="/works">WORKS</a></li>
+            <li><a href="/blog">BLOG</a></li>
+            <li><a href="/contact">CONTACT</a></li>
+        </ul>
+    </nav>
   </div>
 </template>
+
+<style>
+* {
+   box-sizing: border-box;
+}
+
+body {
+   margin: 0;
+   padding: 0;
+   font-family: "Hiragino Kaku Gothic Pro", "ヒラギノ角ゴ Pro W3", メイリオ, Meiryo, "ＭＳ Ｐゴシック", "Helvetica Neue", Helvetica, Arial, sans-serif;
+   background-color: #e6e6e6;
+}
+.header1 {
+    width: 100%;
+    padding: 30px 4% 10px;
+    background-color: #fff;
+    position: fixed;
+    top: 0;
+    display: flex;
+    align-items: center;
+}
+h1 {
+   margin: 0; padding: 0;
+   font-size: 20px;
+}
+a {	
+   text-decoration: none;
+   color: #4b4b4b;
+}
+ul {
+   list-style: none;
+   margin: 0;
+   display: flex;
+}
+li {
+   margin: 0 0 0 15px;
+   font-size: 14px;
+}
+
+nav {
+   margin: 0 0 0 auto;
+}
+</style>
---
+++ pages/works/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <h1>WORKS</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  layout: 'default',
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  animation: 1s appear;
+}
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+}
+</style>
