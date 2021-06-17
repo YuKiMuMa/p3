7daffc0ca65a6dc4ba2141fa25403e60a4f5ae3d
YuKiMuMa <kidosan3.14@gmail.com>
2021-06-17 23:31:55 +0900

    add footer

---
+++ components/footer.vue
@@ -0,0 +1,59 @@
+<template>
+<div class="footer1">
+    <div class="col sqs-col-6 span-6">
+        <div class="sqs-block html-block sqs-block-html" data-block-type="2" id="block-yui_3_17_2_1_1600020919970_27245">
+            <div class="sqs-block-content preSlide slideIn" style="transition-timing-function: ease; transition-duration: 0.8s; transition-delay: 0.5625s;">
+                <p style="text-align: center; white-space: pre-wrap; transition-timing-function: ease; transition-duration: 0.8s; transition-delay: 0.575s;" class="sqsrte-small preFade fadeIn">© 2021 YuKiMuMa</p>
+            </div>
+        </div>
+    </div>
+    <div class="social_icons">
+        <div class="sqs-block-content preSlide slideIn" style="transition-timing-function: ease; transition-duration: 0.8s; transition-delay: 0.5875s;">
+            <div class="sqs-svg-icon--outer social-icon-alignment-center social-icons-color- social-icons-size-extra-small social-icons-style-regular ">
+                <nav class="sqs-svg-icon--list">
+                    <a href="https://www.instagram.com/twistedpoly/" target="_blank" class="sqs-svg-icon--wrapper instagram-unauth" aria-label="">
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" >
+                        <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"/>
+                        </svg>
+                    </div>
+                    </a><a href="https://twitter.com/twistedpoly" target="_blank" class="sqs-svg-icon--wrapper twitter-unauth" aria-label="Twitter">
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" >
+                        <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"/>
+                        </svg>
+                    </div>
+                    </a><a href="https://vimeo.com/twistedpoly" target="_blank" class="sqs-svg-icon--wrapper vimeo-unauth" aria-label="Vimeo">
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" >
+                        <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"/>
+                        </svg>
+                    </div>
+                    </a>
+                </nav>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<style>
+.footer1{
+    width: 100%;
+    padding: 10px 4% 10px;
+    background-color: #fff;
+    display: flex;
+    align-items: center;
+}
+.social_icons{
+    margin: 0 0 0 auto;
+}
+nav{
+    display: flex;
+}
+a{
+    margin:0 0 0 15px;
+}
+
+</style>
+
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,16 +2,19 @@
   <div>
     <header1/>
     <nuxt />
+    <footer1/>
   </div>
 </template>
 
 <script>
 import header1 from '~/components/header.vue'
+import footer1 from '~/components/footer.vue'
 
 export default {
   name: 'default',
   components: {
-    header1
+    header1,
+    footer1
   }
 }
 </script>
