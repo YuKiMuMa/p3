5d56800e7362956790ddf27560f4aa3030761f54
YuKiMuMa <kidosan3.14@gmail.com>
2022-12-21 16:56:34 +0900

    client only4

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -3,9 +3,8 @@
       <div class="title">
         <h1>CONTACT3</h1>
       </div>
-      <validation-observer ref="observer" v-slot="{ invalid, validated }" tag="form" class="p-contact__form" name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field" @submit.prevent="onSubmit" :class="sendingClass">
       <client-only placeholder="Loading…">
-
+      <validation-observer ref="observer" v-slot="{ invalid, validated }" tag="form" class="p-contact__form" name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field" @submit.prevent="onSubmit" :class="sendingClass">
       <div class="p-contact__item">
         <label for="username">お名前</label>
         <validation-provider v-slot="{ errors }" rules="required|max:100" name="お名前">
@@ -48,12 +47,12 @@
       </div>
       <!-- /.p-contact__item -->
 
-      </client-only>
       <div class="p-contact__submit">
         <button type="submit" :disabled="invalid || !validated">送信</button>
       </div>
       <!-- /.p-contact__submit -->
       </validation-observer>
+      </client-only>
     </div>
 </template>
 
