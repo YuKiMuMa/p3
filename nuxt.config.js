263cf55425322f2c0ecec4230abfe81324fb2a82
YuKiMuMa <kidosan3.14@gmail.com>
2022-12-21 17:08:14 +0900

    ssr

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,8 +17,8 @@ export default {
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
   ],
-  ssr:true,
-  target: "static",
+  ssr:false,
+
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
