acd03dd81259f4bed36f65ad6602059fefdf54d8
YuKiMuMa <kidosan3.14@gmail.com>
2022-12-21 21:35:28 +0900

    form test

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,127 +1,80 @@
 <template>
-    <div class="container">
-      <div class="title">
-        <h1>CONTACT3</h1>
-      </div>
-      <validation-observer ref="observer" v-slot="{ invalid, validated }" tag="form" class="p-contact__form" name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field" @submit.prevent="onSubmit" :class="sendingClass">
-
-      <div class="p-contact__item">
-        <label for="username">お名前</label>
-        <validation-provider v-slot="{ errors }" rules="required|max:100" name="お名前">
-          <input type="text" id="username" name="username" v-model="username" autocomplete="name">
-          <p v-show="errors.length" class="p-contact__error">{{ errors[0] }}</p>
-        </validation-provider>
-      </div>
-      <!-- /.p-contact__item -->
-
-      <div class="p-contact__item">
-        <label for="useremail">メールアドレス</label>
-        <validation-provider v-slot="{ errors }" rules="required|email|max:256" name="メールアドレス">
-          <input type="text" id="useremail" name="useremail" v-model="useremail" autocomplete="email">
-          <p v-show="errors.length" class="p-contact__error">{{ errors[0] }}</p>
-        </validation-provider>
-      </div>
-      <!-- /.p-contact__item -->
-
-      <div class="p-contact__item">
-        <label for="message">お問い合わせ内容</label>
-        <validation-provider v-slot="{ errors }" rules="required|max:1000" name="お問い合わせ内容">
-          <textarea id="message" name="message" v-model="message"></textarea>
-          <p v-show="errors.length" class="p-contact__error">{{ errors[0] }}</p>
-        </validation-provider>
-      </div>
-      <!-- /.p-contact__item -->
-
-      <div class="p-contact__item" v-show="false">
-        <label for="message">スパムでない場合は空欄</label>
-        <input type="text" name="bot-field" v-model="botField"/>
-      </div>
-      <!-- /.p-contact__item -->
-
-      <div class="p-contact__submit">
-        <button type="submit" :disabled="invalid || !validated">送信</button>
-      </div>
-      <!-- /.p-contact__submit -->
-      </validation-observer>
-    </div>
+  <section class="contact-container">
+    <template v-if="!finished">
+      <form name="contact" method="POST" data-netlify="true" @submit.prevent>
+        <p>
+          <label>
+            お名前:
+            <input v-model="form.name" type="text" name="name" />
+          </label>
+        </p>
+        <p>
+          <label>
+            メールアドレス:
+            <input v-model="form.email" type="email" name="email" />
+          </label>
+        </p>
+        <p>
+          <label>
+            お問い合わせ内容:
+            <textarea id="form-content" v-model="form.content" name="content" />
+          </label>
+        </p>
+        <p>
+          <button @click="handleSubmit" v-text="'送信'" />
+        </p>
+      </form>
+    </template>
+    <template v-else>
+      <p v-text="'お問い合わせ頂きありがとうございました。'" />
+      <p><nuxt-link to="/" v-text="'TOPへ'" /></p>
+    </template>
+  </section>
 </template>
-
 <script>
-
- export default {
-    data() {
-      return {
-        username        : '',
-        useremail       : '',
-        message         : '',
-        botField        : '',
-        isSubmit        : false,
-        isSending       : false,
-        isError         : false,
-        completeMessage : '',
-      }
-    },
-    computed: {
-      sendingClass(){
-        return {
-          'is-sending'  : this.isSending,
-          'is-error'    : this.isError,
-          'is-complete' : this.isSubmit
-        };
-      }
-    },
-    methods: {
-      onSubmit() {
-        if(this.isSending){
-          return;
-        }
-        this.isSending = true;
-        this.completeMessage = '送信処理中…';
-        const params = new URLSearchParams();
-        params.append('form-name', 'contact');
-        params.append('username', this.username);
-        params.append('useremail', this.useremail);
-        params.append('message', this.message);
-        if(this.botField){
-          params.append('bot-field', this.botField);
-        }
-        this.$axios
-        .$post('/', params)
-        .then(() => {
-          this.completeMessage = 'お問い合わせを送信しました！';
-          this.resetForm();
-          this.isSubmit  = true;
-        })
-        .catch(err => {
-          this.completeMessage = 'お問い合わせの送信が失敗しました';
-          this.isError   = true;
-        })
-        .finally(() => {
-          this.isSending = false;
-        });
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      form: {
+        name: '',
+        email: '',
+        content: ''
       },
-
-      resetForm() {
-        this.username        = '';
-        this.useremail       = '';
-        this.message         = '';
-        this.isError         = false;
-        this.$refs.observer.reset();
-      }
+      finished: false
     }
- }
+  },
+  methods: {
+    encode(data) {
+      return Object.keys(data)
+        .map(
+          (key) => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`
+        )
+        .join('&')
+    },
+    handleSubmit() {
+      const axiosConfig = {
+        header: { 'Content-Type': 'application/x-www-form-urlencoded' }
+      }
+      axios
+        .post(
+          '/',
+          this.encode({
+            'form-name': 'contact',
+            ...this.form
+          }),
+          axiosConfig
+        )
+        .then(() => {
+          this.finished = true
+        })
+    }
+  }
+}
 </script>
-
 <style>
-.container {
-  padding-top: 100px;
-  padding-bottom: 100px;
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  flex-flow: column;
-  justify-content: center;
-  align-items: center;
+.contact-container {
+  padding: 64px;
   text-align: center;
 }
 </style>
