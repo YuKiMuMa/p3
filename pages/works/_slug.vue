d92b2abc0f1eaac6c9aaf2585a6854130eec98bb
YuKiMuMa <kidosan3.14@gmail.com>
2022-12-28 18:39:03 +0900

    add meta for twitter

--- pages/works/_slug.vue
+++ pages/works/_slug.vue
@@ -31,7 +31,9 @@ export default {
         { hid: 'og:description', property: 'og:description', content: 'YuKiMuMa' },
         { hid: 'og:url', property: 'og:url', content: 'https://yukimuma.net/' },
         { hid: 'og:title', property: 'og:title', content: 'YuKiMuMa' },
-        { hid: 'og:image', property: 'og:image', content: "https://yukimuma.net"+this.works.img }
+        { hid: 'og:image', property: 'og:image', content: "https://yukimuma.net"+this.works.img },
+        { name: 'twitter:card', content: 'summary_large_image' },
+        { name: 'twitter:creator', content: 'YuKiMuMa' }
       ],
     }
   },
