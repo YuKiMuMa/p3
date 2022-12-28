a7e6a175195746b709d83933495facf4d65dcb9d
YuKiMuMa <kidosan3.14@gmail.com>
2022-12-28 18:26:12 +0900

    add share button in works

--- pages/works/_slug.vue
+++ pages/works/_slug.vue
@@ -11,6 +11,9 @@
     </div>
     <nuxt-link :to="'/works/'" class="back">← Back to WORKS</nuxt-link>
   </div>
+  <hr>
+  <p>↓シェアしてね↓</p>
+  <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 </div>
 </template>
 
