5b1f9e2e89b353f403f06b9d453edb39b75b4689
YuKiMuMa <kidosan3.14@gmail.com>
2022-12-28 18:31:31 +0900

    fix share button position

--- pages/works/_slug.vue
+++ pages/works/_slug.vue
@@ -9,11 +9,12 @@
         <nuxt-content :document="works" />
       </article>
     </div>
+    <hr>
+    <p>↓シェアしてね↓</p>
+    <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    <h1></h1>
     <nuxt-link :to="'/works/'" class="back">← Back to WORKS</nuxt-link>
   </div>
-  <hr>
-  <p>↓シェアしてね↓</p>
-  <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 </div>
 </template>
 
